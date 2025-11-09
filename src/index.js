f82e8d165a4f9369a2c384afb508d75dbfeaeb7c
ShahzadKhichi <shahzadkhichi996@gmail.com>
2025-11-09 09:50:24 +0500

    basic structure

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-const { PORT } = require("./config");
+const { serverConfig, logger } = require("./config");
 
 const express = require("express");
 
 const app = express();
 
-app.listen(PORT, () => console.log("server is running on port : " + PORT));
+app.use(express.json());
+
+//routes import
+
+const router = require("./routes/index");
+
+//routes
+
+app.use("/api", router);
+
+app.listen(serverConfig.PORT, () => {
+  console.log("server is running on port : " + serverConfig.PORT);
+  logger.info("successfull started the sever", "root", {});
+});
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -2,4 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
+const { infoController } = require("../../controllers/index");
+
+router.get("/info", infoController.info);
+
 module.exports = router;
